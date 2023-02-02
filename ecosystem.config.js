41c79072f9bdd2f0e7316ec70d6bf9e395da1d79
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-02-02 17:19:17 +0300

    cwd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,24 @@
 module.exports = {
   apps : [
-    {"name": "aggregator", "script": "yarn --cwd ./webapps/aggregator && yarn --cwd ./webapps/aggregator start"},
-    {"name": "navbar", "script": "yarn --cwd ./webapps/navbar && yarn --cwd ./webapps/navbar start"},
-    {"name": "services", "script": "yarn --cwd ./webapps/services && yarn --cwd ./webapps/services start"},
-    {"name": "timetable", "script": "yarn --cwd ./webapps/timetable && yarn --cwd ./webapps/timetable start"},
+    {
+      "name": "aggregator",
+      "cwd": "./webapps/aggregator",
+      "script": "yarn && yarn start"
+    },
+    {
+      "name": "navbar",
+      "cwd": "./webapps/navbar",
+      "script": "yarn && yarn start"
+    },
+    {
+      "name": "services",
+      "cwd": "./webapps/services",
+      "script": "yarn && yarn start"
+    },
+    {
+      "name": "timetable",
+      "cwd": "./webapps/timetable",
+      "script": "yarn && yarn start"
+    },
   ],
 };
