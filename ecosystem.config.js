f972edefacfb7be725b2ca51a995861a257353be
Artem Netsvetaev <physphile@vk.com>
2023-02-11 13:15:31 +0300

    lint again

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,34 +1,34 @@
 module.exports = {
-  apps: [
-    {
-      name: "aggregator",
-      cwd: './webapps/aggregator',
-      script: "yarn && yarn start",
-    },
-    {
-      name: "navbar",
-      cwd: './webapps/navbar',
-      script: "yarn && yarn start",
-    },
-    {
-      name: "services",
-      cwd: './webapps/services',
-      script: "yarn && yarn start",
-    },
-    {
-      name: "timetable",
-      cwd: './webapps/timetable',
-      script: "yarn && yarn start",
-    },
-    {
-      name: "toolbar",
-      cwd: './webapps/toolbar',
-      script: "yarn && yarn start",
-    },
-    {
-      name: "devtools",
-      cwd: './webapps/devtools',
-      script: "yarn && yarn start",
-    },
-  ],
+    apps: [
+        {
+            name: 'aggregator',
+            cwd: './webapps/aggregator',
+            script: 'yarn && yarn start',
+        },
+        {
+            name: 'navbar',
+            cwd: './webapps/navbar',
+            script: 'yarn && yarn start',
+        },
+        {
+            name: 'services',
+            cwd: './webapps/services',
+            script: 'yarn && yarn start',
+        },
+        {
+            name: 'timetable',
+            cwd: './webapps/timetable',
+            script: 'yarn && yarn start',
+        },
+        {
+            name: 'toolbar',
+            cwd: './webapps/toolbar',
+            script: 'yarn && yarn start',
+        },
+        {
+            name: 'devtools',
+            cwd: './webapps/devtools',
+            script: 'yarn && yarn start',
+        },
+    ],
 };
